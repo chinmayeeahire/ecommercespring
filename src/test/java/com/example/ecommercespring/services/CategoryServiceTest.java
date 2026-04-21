c7287ac2b83f59d5d8df5885bc4d2fd2a689f89c
chinmayeeahire <cheeni1001@gmail.com>
2026-04-21 17:27:59 +0530

    added unit test for  "should return all categories succesfully"

---
+++ src/test/java/com/example/ecommercespring/services/CategoryServiceTest.java
@@ -0,0 +1,66 @@
+package com.example.ecommercespring.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.ecommercespring.DTO.CategoryDTO;
+import com.example.ecommercespring.Entity.Category;
+import com.example.ecommercespring.Repository.CategoryRepository;
+import com.example.ecommercespring.Services.CategoryService;
+
+@ExtendWith(MockitoExtension.class) //This annotation is used to enable Mockito for the test class with Junit
+public class CategoryServiceTest {
+    
+    @Mock
+    private CategoryRepository categoryRepository;
+    
+
+    @InjectMocks
+    private CategoryService categoryService;
+
+    @Test
+    @DisplayName("should return all categories succesfully")
+    void getAllCategories_shouldReturnAllCategories(){
+       //arrange
+       List<Category> categories=new ArrayList<>();
+       Category category1 = Category.builder()
+        .name("Electronics")
+        .build();
+       category1.setId(1L);
+      Category category2 = Category.builder()
+        .name("Books")
+        .build();
+       category2.setId(2L);
+
+      Category category3 = Category.builder()
+        .name("Movies")
+        .build();
+       category2.setId(2L);
+
+       categories.add(category1);
+       categories.add(category2);
+       categories.add(category3);
+        when(categoryRepository.findAll()).thenReturn(categories);
+       //act
+
+       List<CategoryDTO> result=categoryService.getAllCategories();
+
+
+       //assert
+
+      assertEquals(result.size(), 3);
+      verify(categoryRepository, times(1)).findAll();
+    }
+}
